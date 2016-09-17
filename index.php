77ed60dee1457dbe3aaefeb9d147327903a4fcd4
drsherlock <zain.delhi27@hotmail.com>
2016-09-17 17:14:02 +0530

    Add index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dr_sherlock
+ * Date: 17/9/16
+ * Time: 5:00 PM
+ */
+
+
+?>
+
+<h1>UNOME</h1>
+<h2>Keep Your Thoughts Safe With Unome</h2>
+<form method="get" action="login_form.php">
+    <button type="submit">Login</button>
+</form>
+<form method="get" action="register_form.php">
+    <button type="submit">Register</button>
+</form>
